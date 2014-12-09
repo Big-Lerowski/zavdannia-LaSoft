c215ae9793dc7ae7a4ec351fcfebc8559e5ebc73
Ho-Lera <rusavskaV@gmail.com>
2014-12-09 11:51:50 +0200

    test commit

---
+++ 0.js
@@ -0,0 +1,19 @@
+0. function add (){
+ for(var i=1;i<arguments.length;i++){
+  arguments[0]+=arguments[i];
+ }
+ return arguments[0];
+};
+add(1,2,4,5,8);
+add(1, 4, 5, 5, 3, 5, 22, 5, 4, 65, 5, 42, 77);
+add(1,2);
+ 
+
+ 
+
+ 
+
+
+    
+    
+   
---
+++ 1.js
@@ -0,0 +1,28 @@
+1.function fib (){
+ var m=[0,1];
+ for(var i=2;i<12;i++){
+  m[i]=m[i-2]+m[i-1];
+ }
+ return m;
+}; fib() - //prosto 12 pershuh elementiv
+ 
+//OR: 1. var i=2;
+var m=[0,1];
+m.length=12;
+function fib (){ 
+ m[i]=m[i-2]+m[i-1];
+ i++;
+ if(i<m.length-1){ 
+  fib ();
+ }
+ return m;
+}; fib() - //iaksho kojnogo razy vuklukatu funkcijy, to dodaetsia po odnomy elementu z riady fibonachi.
+ 
+//OR: 1. function fib (n){
+ var m=[0,1];
+ for(var i=2;i<n;i++){
+  m[i]=m[i-2]+m[i-1];
+ }
+ return m;
+}; fib(455) - //vukluche 455 elementiv z tiady fibonachi.
+
