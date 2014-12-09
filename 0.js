47deb58d1ca1db2d5d978c1c5008dcb393694e21
Ho-Lera <rusavskaV@gmail.com>
2014-12-09 12:41:23 +0200

    test5

--- 0.js
+++ 0.js
@@ -1,30 +1,22 @@
-1.function fib (){
- var m=[0,1];
- for(var i=2;i<12;i++){
-  m[i]=m[i-2]+m[i-1];
- }
- return m;
-}; fib() - //prosto 12 pershuh elementiv
- 
-//OR: 1. var i=2;
-var m=[0,1];
-m.length=12;
-function fib (){ 
- m[i]=m[i-2]+m[i-1];
- i++;
- if(i<m.length-1){ 
-  fib ();
- }
- return m;
-}; fib() - //iaksho kojnogo razy vuklukatu funkcijy, to dodaetsia po odnomy elementu z riady fibonachi.
- 
-//OR: 1. function fib (n){
- var m=[0,1];
- for(var i=2;i<n;i++){
-  m[i]=m[i-2]+m[i-1];
- }
- return m;
-}; fib(455) - //vukluche 455 elementiv z tiady fibonachi.
+4. // dyvno, ale praciue
+function populate(obj){
+   var obj = {
+          };
+        }
+    populate(obj);
+    console.log(obj);
+    
+    //OR:
+    
+    function populate(name,password){
+   var obj = {
+   name:"name",
+  password:"password";
+             };
+                          }
+    populate(obj);
+    console.log(obj);
+    // tyt y mene e putannia
 
 
 
